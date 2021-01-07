eadbdabe0211c2c91a8690381c9c2f451c624779
Dimitri Subrini <dimitri.subrini@gmail.com>
2021-01-07 14:14:19 +0100

    redo the navigation to highlight the anchors elements according to the height in the page

--- js/script.js
+++ js/script.js
@@ -2,19 +2,14 @@ import frontTechnos from './data/front.technos.js';
 import backTechnos from './data/back.technos.js';
 import toolsTechnos from './data/tools.technos.js';
 
+const wrapperElement = document.querySelector('.wrapper');
 const frontElement = document.getElementById('front');
 const backElement = document.getElementById('back');
 const toolsElement = document.getElementById('tools');
+const parallaxSections = document.querySelectorAll('.parallax');
 
 const navLinks = document.getElementsByClassName('nav_links');
 const arrayNavLinks = Array.from(navLinks);
-
-arrayNavLinks.forEach((navItem) => {
-  navItem.addEventListener('click', () => {
-    arrayNavLinks.map((item) => item.classList.remove('current-link'));
-    navItem.classList.toggle('current-link');
-  });
-});
 
 const createCardElement = (data, parentElement) => {
   data.map((item) => {
@@ -34,3 +29,24 @@ const createCardElement = (data, parentElement) => {
 createCardElement(frontTechnos, frontElement);
 createCardElement(backTechnos, backElement);
 createCardElement(toolsTechnos, toolsElement);
+
+const parallaxArray = {};
+let i = 0;
+
+Array.prototype.forEach.call(parallaxSections, function (e) {
+  parallaxArray[e.id] = e.offsetTop - 500;
+});
+console.log(parallaxArray);
+
+wrapperElement.onscroll = function () {
+  let scrollPosition = wrapperElement.scrollTop;
+
+  for (i in parallaxArray) {
+    if (parallaxArray[i] <= scrollPosition) {
+      document.querySelector('.current-link').setAttribute('class', ' ');
+      document
+        .querySelector('a[href*=' + i + ']')
+        .setAttribute('class', 'current-link');
+    }
+  }
+};
